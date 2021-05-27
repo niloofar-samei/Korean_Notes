cbaf32880f9b439115bf0e092b322dd4200a286f
niloofar-samei <niloofar.samei@gmail.com>
2021-05-27 20:45:38 +0430

    Creating admin user. Generating admin site.

--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-class notes(models.Model):
+class Notes(models.Model):
     note_title = models.CharField(max_length=100)
     note_text = models.CharField(max_length=500)
     pud_date = models.DateTimeField('date published')
     author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.note_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(deys=1)
