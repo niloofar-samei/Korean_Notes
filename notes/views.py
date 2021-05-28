1f4fd7575b457900bc4940fcc3ddae93122c886c
niloofar-samei <niloofar.samei@gmail.com>
2021-05-28 09:20:54 +0430

    Adding template. Creating simple test html file for index.

--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Notes
 
 # Create your views here.
 def index(request):
     latest_quick_notes = Notes.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.note_text for q in latest_quick_notes])
-    return HttpResponse(output)
+    context = {'latest_quick_notes': latest_quick_notes}
+    return render(request, 'notes/index.html', context)
 
 def note(request, id):
     response = "You're looking at the note %s."
