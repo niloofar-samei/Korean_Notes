3fdba8d43b11ef6eb2cd0bc2d1de9d55c5b42afd
niloofar-samei <niloofar.samei@gmail.com>
2021-06-15 01:34:01 +0430

    Showing related foreign key in Comments' edit section inside Admin panel.

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Note, Comment
 
+class CommentAdmin(admin.ModelAdmin):
+        raw_id_fields = ['note']
+
+try:
+        admin.site.unregister(Comment)
+except: pass
+admin.site.register(Comment,CommentAdmin)
 admin.site.register(Note)
-admin.site.register(Comment)
